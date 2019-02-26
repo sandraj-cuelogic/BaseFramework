e676da3ea0c959aa5cd6cd047d284bac48c371ed
Sandra John <sandra.john@zs.com>
2019-02-26 17:43:08 +0530

    Unwanted print statements removed

--- src/main/java/com/baseframework/automationFramework/TestLinkStatusUpdate.java
+++ src/main/java/com/baseframework/automationFramework/TestLinkStatusUpdate.java
@@ -12,10 +12,6 @@ public class TestLinkStatusUpdate {
 	
 	public void updateTestLinkResultWithExecutionNote(String PLAN_NAME, String BUILD_NAME, String EXECUTION_COMMENT, String testCase, String exception, String result) throws TestLinkAPIException{
 		
-		System.out.println(DEV_KEY);
-		System.out.println(SERVER_URL);
-		System.out.println(PROJECT_NAME);
-		
 		TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEV_KEY, SERVER_URL);
 		AutomationLog.info("TestLink Connection has been established : " +testlinkAPIClient.isConnected);
 		//With comment for the execution
@@ -25,10 +21,6 @@ public class TestLinkStatusUpdate {
 	
 	public void updateTestLinkResultWithoutExecutionNote(String PLAN_NAME, String BUILD_NAME, String testCase, String exception, String result) throws TestLinkAPIException{
 		
-		System.out.println(DEV_KEY);
-		System.out.println(SERVER_URL);
-		System.out.println(PROJECT_NAME);
-		
 		TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEV_KEY, SERVER_URL);
 		AutomationLog.info("TestLink Connection has been established : " +testlinkAPIClient.isConnected);
 		//Without comment for the execution
@@ -36,10 +28,6 @@ public class TestLinkStatusUpdate {
 	}
 	
 	public void updateTestLinkResultWithBugIDAndNotes(int PLAN_ID, int BUILD_ID, int testCaseID, Boolean guess, String execNotes, String result, int bugID) throws TestLinkAPIException{
-		
-		System.out.println(DEV_KEY);
-		System.out.println(SERVER_URL);
-		System.out.println(PROJECT_NAME);
 		
 		TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEV_KEY, SERVER_URL);
 		AutomationLog.info("TestLink Connection has been established : " +testlinkAPIClient.isConnected);
